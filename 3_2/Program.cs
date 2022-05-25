645ad339d8e822a4019e36498dd14a1dc8730a6b
cleyk112 <106143536+cleyk112@users.noreply.github.com>
2022-05-25 09:31:51 +0200

    Add files via upload

---
+++ 3_2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace Lab3_2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> map = new Dictionary<int, string>()
+            {
+                [0] = "S001",
+                [1] = "S002",
+                [2] = "S001",
+                [3] = "S009",
+                [4] = "S005",
+                [5] = "S005",
+                [6] = "S007"
+
+
+
+            };
+
+            Console.WriteLine("Starting Dictionary");
+            foreach (var item in map)
+            {
+                Console.WriteLine($"({item.Key} : {item.Value})");
+            }
+
+            List<string> uniqvall = new List<string>();
+           
+            //removing duplicates
+            Console.WriteLine("Unique values: ");
+            for (int i = 0; i < map.Count; i++)
+            {
+                if (!uniqvall.Contains(map[i]))  
+                    uniqvall.Add(map[i]); 
+            }
+
+            Console.WriteLine(String.Join(",", uniqvall));
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Result of serialization:");
+            string jsonString = JsonSerializer.Serialize(uniqvall);
+            File.WriteAllText("Dictionary.json", jsonString);  //serialization
+            Console.WriteLine(jsonString);
+
+        }
+    }
+}
---
+++ Lab3-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] stringArray = { "Hello i am 17", "$20 is a price value" , "twenty two three five"  
+                                                     , "44 years in custody"  , "random set of words " };
+
+            Console.WriteLine(String.Join(Environment.NewLine, stringArray));
+
+            var evenrow = stringArray.Where(x => x.Length % 2 == 0).Select(x => x.First()).ToArray();
+            var oddrow = stringArray.Where(x => x.Length % 2 == 1).Select(x => x.Last()).ToArray();
+
+            var result = evenrow.Concat(oddrow).ToArray();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Unsorted row: ");
+            Console.WriteLine(String.Join(" ", result));
+            Console.WriteLine(" ");
+            //
+            Array.Sort(result, StringComparer.Ordinal);
+
+            Console.WriteLine("Sorted row: ");
+            Console.WriteLine(String.Join(" ",result));
+
+        }
+    }
+}
